020708efefcfd1689157d84928d08c665c89ad09
Stan Lewis <gashcrumb@gmail.com>
2024-10-30 11:26:27 -0400

    chore: tidy up the sidebar item props
    Signed-off-by: Stan Lewis <gashcrumb@gmail.com>

--- src/components/SimpleSidebarItem/SimpleSidebarItem.tsx
+++ src/components/SimpleSidebarItem/SimpleSidebarItem.tsx
@@ -1,21 +1,21 @@
 import React, { useCallback, useState } from "react";
 import { SidebarItem } from "@backstage/core-components";
 import { SimpleDialog } from "../../plugin";
-import { IconComponent } from "@backstage/core-plugin-api";
 import { SportsBar } from "@mui/icons-material";
 
 export type SimpleSidebarItemProps = {
-  icon?: IconComponent;
+  to: string;
   text?: string;
 };
 
-export const SimpleSidebarItem = ({ text }: SimpleSidebarItemProps) => {
+export const SimpleSidebarItem = ({ text, to }: SimpleSidebarItemProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const handleClose = useCallback(() => {
     setIsOpen(false);
   }, [isOpen, setIsOpen]);
   return (
     <SidebarItem
+      to={to}
       onClick={() => setIsOpen(!isOpen)}
       text={text}
       icon={SportsBar}
